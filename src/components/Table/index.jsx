cb8d1a785b81853ca563b442a39392cbae65d688
Victor <victorsmirnov@me.com>
2022-08-23 12:10:37 +0300

    cod formatting

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -9,7 +9,8 @@ import './style.sass';
 
 const Table = () => {
 
-  const [ table, setTable ] = useState(goodies.map(goodie => 
+  const [ table, setTable ] = useState(
+    goodies.map(goodie => 
       ({ 
         ...goodie, 
         priceTotal: goodie.price * goodie.count,
@@ -17,64 +18,71 @@ const Table = () => {
     )
   );
 
-  const [ totals, setTotals ] = useState({
-    totalPrice: table.reduce((prev, curr) => prev + curr.priceTotal, 0),
-    totalCount: table.reduce((prev, curr) => prev + curr.count, 0),
-  });
-
-  useEffect(() => setTotals({
-    totalPrice: table.reduce((prev, curr) => prev + curr.priceTotal, 0),
-    totalCount: table.reduce((prev, curr) => prev + curr.count, 0),
-  }), [table]);
-
-  const incrise = id => setTable(table => table.map(goodie => 
-    goodie.id === id ? 
-    { ...goodie,
-      count: ++goodie.count, 
-      priceTotal: goodie.price * goodie.count,
-    } : 
-    goodie)
+  const [ totals, setTotals ] = useState(
+    {
+      totalPrice: table.reduce((prev, curr) => prev + curr.priceTotal, 0),
+      totalCount: table.reduce((prev, curr) => prev + curr.count, 0),
+    }
   );
 
-  const decrise = id => setTable(table => table.map(goodie => 
-    goodie.id === id ? 
-    { ...goodie,
-      count: goodie.count > 1 ? --goodie.count : goodie.count, 
-      priceTotal: goodie.price * goodie.count,
-    } : 
-    goodie)
+  useEffect(() => setTotals(
+    {
+      totalPrice: table.reduce((prev, curr) => prev + curr.priceTotal, 0),
+      totalCount: table.reduce((prev, curr) => prev + curr.count, 0),
+    }
+  ), [table]);
+
+  const incrise = id => setTable(table => 
+    table.map(goodie => 
+      goodie.id === id ? 
+      {
+        ...goodie,
+        count: ++goodie.count, 
+        priceTotal: goodie.price * goodie.count,
+      } : 
+      goodie)
   );
 
-  const changeValue = (id, value) => 
-    setTable(table => 
-      table.map(goodie => 
-        id === goodie.id ? 
-        { 
-          ...goodie, 
-          count: value,
-          priceTotal: goodie.price * value,
-        } : 
-        goodie)
-    );
+  const decrise = id => setTable(table => 
+    table.map(goodie => 
+      goodie.id === id ? 
+      { 
+        ...goodie,
+        count: goodie.count > 1 ? --goodie.count : goodie.count, 
+        priceTotal: goodie.price * goodie.count,
+      } : 
+      goodie)
+  );
 
-  const deleteGoogie = id => setTable(table => table.filter(goodie => id !== goodie.id));
+  const changeValue = (id, value) => setTable(table => 
+    table.map(goodie => 
+      id === goodie.id ? 
+      { 
+        ...goodie, 
+        count: value,
+        priceTotal: goodie.price * value,
+      } : 
+      goodie)
+  );
 
-  const rows = table.map( 
-    goodie => 
-      <TableRow 
-        key={ goodie.id } 
-        goodie={ goodie } 
-        deleteGoogie={ deleteGoogie } 
-        incrise={ incrise }
-        decrise={ decrise }
-        changeValue={ changeValue }
-      />
+  const deleteGoogie = id => setTable(table => 
+    table.filter(goodie => id !== goodie.id)
   );
 
   return (
     <div className="section-cart__table">
       <TableHeader />
-      { rows }
+      { table.map( 
+          goodie => 
+            <TableRow 
+              key={ goodie.id } 
+              goodie={ goodie } 
+              deleteGoogie={ deleteGoogie } 
+              incrise={ incrise }
+              decrise={ decrise }
+              changeValue={ changeValue }
+            />
+      )}
       <TableFooter totals={ totals } />
     </div>
   );
